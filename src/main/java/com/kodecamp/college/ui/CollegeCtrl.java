01a1f8f5f1afd29c6ea36c8c8aeb150193a93829
kodecamp <kodecamp.in@gmail.com>
2019-02-13 11:04:25 +0530

    Google Checkstyle Integration

--- src/main/java/com/kodecamp/college/ui/CollegeCtrl.java
+++ src/main/java/com/kodecamp/college/ui/CollegeCtrl.java
@@ -1,56 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.kodecamp.college.ui;
 
 import com.kodecamp.college.db.College;
 import com.kodecamp.commons.ui.Message;
 import com.kodecamp.commons.ui.MessageUtil;
 import com.kodecamp.commons.ui.ValidationUtil;
+
 import java.io.IOException;
+
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
+
 
 // simple class
 public class CollegeCtrl {
+  private static final String VIEW_PATH_PREFIX = "/views/college";
   // fields
 
-  private String value1 = "Value 1";
-  private String value2 = "Value 2";
-  private String value3 = "Value 3";
-  private String value4 = "Value 4 : This value is coming from controller";
+  private final String value1 = "Value 1";
+  private final String value2 = "Value 2";
+  private final String value3 = "Value 3";
+  private final String value4 = "Value 4 : This value is coming from controller";
 
-  private static final String viewPathPrefix = "/views/college";
-  private final List<UICollege> collegeList = new ArrayList<>();
+  private final List<UICollege> collegeList = new ArrayList<>(10);
 
+  /**
+   * <p>
+   * This method will be used for urls like http://context/college/list.xhtml.
+   * </p>
+   *
+   * @param req instance of <tt>HttpServletRequest</tt>
+   * @param resp instance of <tt>HttpServletResponse</tt>
+   * @return list view
+   */
   public String list(HttpServletRequest req, HttpServletResponse resp) {
-//    final HttpSession session = req.getSession();
-//    final List<UICollege> list = (List<UICollege>) session.getAttribute("collegeList");
-    final String contextPath = req.getContextPath();
     if (this.collegeList.isEmpty()) {
       this.collegeList.addAll(createDummyList(req, resp));
     }
-//      session.setAttribute("collegeList", colleges);
-    return this.viewPathPrefix + "/college_list.jsp";
+    return VIEW_PATH_PREFIX + "/college_list.jsp";
   }
 
-  private List<UICollege> createDummyList(final HttpServletRequest request, final HttpServletResponse resp) {
+  /**
+   * <p>
+   * This method will be used for urls like http://context/college/list.xhtml.
+   * </p>
+   *
+   * @param req instance of <tt>HttpServletRequest</tt>
+   * @param resp instance of <tt>HttpServletResponse</tt>
+   * @return list view
+   */
+  private List<UICollege> createDummyList(final HttpServletRequest request,
+          final HttpServletResponse resp) {
     final String contextPath = request.getContextPath();
-    final List<UICollege> dummyColleges = new ArrayList<>();
-    final UICollege college1 = new UICollege(new College("sar001", "Saroj Institute", "Lucknow"), contextPath);
+    final List<UICollege> dummyColleges = new ArrayList<>(10);
+    final UICollege college1 = new UICollege(
+            new College("sar001", "Saroj Institute", "Lucknow"), contextPath);
     college1.setDeleteUrl(resp.encodeURL(college1.getDeleteUrl()));
     college1.setUpdateUrl(resp.encodeURL(college1.getUpdateUrl()));
 
-    final UICollege college2 = new UICollege(new College("bbd001", "Babu Banarasi Das", "Lucknow"), contextPath);
+    final UICollege college2 = new UICollege(
+            new College("bbd001", "Babu Banarasi Das", "Lucknow"), contextPath);
     college2.setDeleteUrl(resp.encodeURL(college2.getDeleteUrl()));
     college2.setUpdateUrl(resp.encodeURL(college2.getUpdateUrl()));
 
-    final UICollege college3 = new UICollege(new College("azd001", "Azad Institue", "Kanpur"), contextPath);
+    final UICollege college3 = new UICollege(
+            new College("azd001", "Azad Institue", "Kanpur"), contextPath);
     college3.setDeleteUrl(resp.encodeURL(college3.getDeleteUrl()));
     college3.setUpdateUrl(resp.encodeURL(college3.getUpdateUrl()));
 
@@ -60,23 +76,42 @@ public class CollegeCtrl {
     return dummyColleges;
   }
 
+  /**
+   * <p>
+   * This method will be used for urls like http://context/college/list.xhtml.
+   * </p>
+   *
+   * @param req instance of <tt>HttpServletRequest</tt>
+   * @param resp instance of <tt>HttpServletResponse</tt>
+   * @return list view
+   */
   public String addform(final HttpServletRequest req, final HttpServletResponse resp) {
-//    MessageUtil.clearMessages(req);
-    return this.viewPathPrefix + "/college_form.jsp";
+    return this.VIEW_PATH_PREFIX + "/college_form.jsp";
   }
 
+  /**
+   * <p>
+   * This method will be used for urls like http://context/college/list.xhtml.
+   * </p>
+   *
+   * @param req instance of <tt>HttpServletRequest</tt>
+   * @param resp instance of <tt>HttpServletResponse</tt>
+   * @return list view
+   */
   public String addnew(final HttpServletRequest req, final HttpServletResponse resp) {
     String regNo = req.getParameter("regNo");
     String name = req.getParameter("name");
     String address = req.getParameter("address");
 
     if (isValidOrDuplicate(req)) {
-      MessageUtil.setMessages(req, new Message(Message.MSG_TYPE_ERROR, "Please enter the mandatory(*) fields."));
+      MessageUtil.setMessages(req,
+              new Message(Message.MSG_TYPE_ERROR, "Please enter the mandatory(*) fields."));
       return "/college/addform.xhtml?" + "regNo=" + regNo + "&name=" + name + "&address=" + address;
     }
 
     if (this.collegeList.contains(new UICollege(new College(regNo, name, address), ""))) {
-      MessageUtil.setMessages(req, new Message(Message.MSG_TYPE_ERROR, "Already exists. Please enter the new record."));
+      MessageUtil.setMessages(req,
+              new Message(Message.MSG_TYPE_ERROR, "Already exists. Please enter the new record."));
       return "/college/addform.xhml?" + "regNo=" + regNo + "&name=" + name + "&address=" + address;
     }
 
@@ -94,15 +129,17 @@ public class CollegeCtrl {
     final boolean isNameEmpty = ValidationUtil.isEmpty(name);
     final boolean isAddressEmpty = ValidationUtil.isEmpty(address);
 
-//	    final boolean isAnyEmpty = ValidationUtil.isAnyEmpty(isNameEmpty, isAddressEmpty);
-    final boolean isAnyEmpty = (ValidationUtil.isAnyEmpty(isRegNoEmpty, isNameEmpty, isAddressEmpty) || ValidationUtil.isPresent(new UICollege(new College(regNo), ""), this.collegeList));
+    final boolean isAnyEmpty = (ValidationUtil.isAnyEmpty(isRegNoEmpty, isNameEmpty, isAddressEmpty)
+            || ValidationUtil.isPresent(new UICollege(new College(regNo), ""), this.collegeList));
     System.out.println("Value of IS ANY EMPTY  " + isAnyEmpty);
     return isAnyEmpty;
   }
 
   private String addValues(final HttpServletRequest req, final HttpServletResponse resp) {
 
-    final UICollege newCollege = new UICollege(new College(req.getParameter("regNo"), req.getParameter("name"), req.getParameter("address")), req.getContextPath());
+    final UICollege newCollege = new UICollege(
+            new College(req.getParameter("regNo"),
+                    req.getParameter("name"), req.getParameter("address")), req.getContextPath());
     newCollege.setDeleteUrl(resp.encodeURL(newCollege.getDeleteUrl()));
     newCollege.setUpdateUrl(resp.encodeURL(newCollege.getUpdateUrl()));
     this.collegeList.add(newCollege);
@@ -110,15 +147,33 @@ public class CollegeCtrl {
     return ":/college/list.xhtml";
   }
 
+  /**
+   * <p>
+   * This method will be used for urls like http://context/college/list.xhtml.
+   * </p>
+   *
+   * @param request instance of <tt>HttpServletRequest</tt>
+   * @param response instance of <tt>HttpServletResponse</tt>
+   * @return list view
+   */
   public String updateform(final HttpServletRequest request, final HttpServletResponse response) {
 
     final UICollege college = this.collegeList.get(this.collegeList.indexOf(
             new UICollege(new College(request.getParameter("regNo")), null)));
     request.setAttribute("uicollege", college);
 
-    return this.viewPathPrefix + "/college_update_form.jsp";
+    return VIEW_PATH_PREFIX + "/college_update_form.jsp";
   }
 
+  /**
+   * <p>
+   * This method is used for updating the college record.
+   * </p>
+   *
+   * @param req <tt> HttpServletRequest </tt>
+   * @param resp <tt> HttpServletResponse </tt>
+   * @return url to be redirected on
+   */
   public String update(final HttpServletRequest req, final HttpServletResponse resp) {
 
     UICollege collegeObj = new UICollege(new College(req.getParameter("regNo")), "");
@@ -141,13 +196,15 @@ public class CollegeCtrl {
   }
 
   private String onSuccess(final HttpServletRequest req) {
-    MessageUtil.setMessages(req, new Message(Message.MSG_TYPE_INFO, "Record Updated Successfully."));
+    MessageUtil.setMessages(req, new Message(
+            Message.MSG_TYPE_INFO, "Record Updated Successfully."));
     System.out.println("OnSuccess : Messages : " + req.getAttribute("messages"));
     return ":/college/list.xhtml";
   }
 
   private String onFailure(final HttpServletRequest req) {
-    MessageUtil.setMessages(req, new Message(Message.MSG_TYPE_ERROR, "Please fill the reqired (*) fields."));
+    MessageUtil.setMessages(req,
+            new Message(Message.MSG_TYPE_ERROR, "Please fill the reqired (*) fields."));
     System.out.println("OnFailure : Messages : " + req.getAttribute("messages"));
     return "/college/updateform.xhtml?regNo=" + req.getParameter("regNo");
   }
@@ -167,7 +224,18 @@ public class CollegeCtrl {
 
   }
 
-  public String delete(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
+  /**
+   * <p>
+   * This method is used for updating the college record.
+   * </p>
+   *
+   * @param req <tt> HttpServletRequest </tt>
+   * @param resp <tt> HttpServletResponse </tt>
+   * @return url to be redirected on
+   * @throws java.io.IOException throws exception if something goes wrong
+   */
+  public String delete(final HttpServletRequest req,
+          final HttpServletResponse resp) throws IOException {
 
     final String regNo = req.getParameter("regNo");
     UICollege clgObj = new UICollege(new College(regNo), "");
