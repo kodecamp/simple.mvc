49533205e84c6e225e04d64c73d800b867ce344e
kodecamp <kodecamp.in@gmail.com>
2019-02-14 10:59:36 +0530

    junit included

---
+++ src/test/java/com/kodecamp/college/ui/UICollegeTest.java
@@ -0,0 +1,43 @@
+package com.kodecamp.college.ui;
+
+import com.kodecamp.college.db.College;
+import junit.framework.TestCase;
+
+/**
+ * This is the unit test for UICollege.
+ *
+ * @author kcamp
+ */
+public final class UICollegeTest extends TestCase {
+
+  /**
+   * constructor.
+   *
+   * @param testName testname
+   */
+  public UICollegeTest(final String testName) {
+    super(testName);
+  }
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    super.tearDown();
+  }
+
+  /**
+   * Test of getCollege method, of class UICollege.
+   */
+  public void testGetCollege() {
+    System.out.println("getCollege");
+
+    assertEquals(true, true);
+
+  }
+
+
+}
